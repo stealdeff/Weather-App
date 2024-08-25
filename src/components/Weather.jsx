cbdccdef52c0d960eb068eab3041f6106b707297
arina <ukam.naletko@mail.ru>
2024-08-25 13:44:56 +0300

    weather writing comp

---
+++ src/components/Weather.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const WeatherApp = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const apiKey = '675bc84edff6e3cc3a58d263b6d82b1f';
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Ошибка при получении данных о погоде:', error);
+      setWeatherData(null);
+    }
+  };
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Введите название города"
+          value={city}
+          onChange={handleCityChange}
+        />
+        <button type="submit">Показать погоду</button>
+      </form>
+
+      {weatherData ? (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>Температура: {weatherData.main.temp}°C</p>
+          <p>Погода: {weatherData.weather[0].description}</p>
+          <p>Влажность: {weatherData.main.humidity}%</p>
+          <p>Скорость ветра: {weatherData.wind.speed} м/с</p>
+        </div>
+      ) : (
+        <p>Загрузка...</p>
+      )}
+    </div>
+  );
+};
+
+export default WeatherApp;
