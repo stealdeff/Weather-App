801d51bc661b87cd94266d5437836d8d4054ecdf
Arina <Ukam.naletko@mail.ru>
2024-09-08 23:38:20 +0300

    nav button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./components/Home";
 import WeatherCity from "./components/CityWeather";
-import './App.css'; 
 import WeatherLatLon from "./components/LatLonWeather";
+import './App.css'; 
 
 const App = () => {
-  const [isHeaderVisible, setIsHeaderVisible] = useState(false);
+  const [isNavVisible, setIsNavVisible] = useState(false); 
 
   useEffect(() => {
     const header = document.querySelector('.header');
@@ -18,39 +18,40 @@ const App = () => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+
   return (
     <Router>
-      <div className="App">
-      <button 
-          className="toggle-header" 
-          onClick={() => setIsHeaderVisible(!isHeaderVisible)}
-        >
-          {isHeaderVisible ? 'Скрыть хэдер' : 'Показать хэдер'}
-        </button>
-        {isHeaderVisible && (
+      <div className="App">      
         <div className="header">
           <nav>
             <a href="#" className="logo">Weather App</a>
-            <div className="nav-links">
-              <ul>
-                <li><Link to="/Home">Home</Link></li>
-                <li><Link to="/CityWeather">Прогноз погоды в городе</Link></li>
-                <li><Link to="/LatLonWeather">Прогноз по координатам</Link></li>
-                <li><Link to="#">About</Link></li>
-                <li><Link to="#">Connect</Link></li>
-              </ul>
-            </div>
+            <button 
+          className="toggle-nav" 
+          onClick={() => setIsNavVisible(!isNavVisible)}
+        >
+          {isNavVisible ? 'Скрыть навигацию' : 'Показать навигацию'}
+        </button>
+
+            {isNavVisible && (
+              <div className="nav-links">
+                <ul>
+                  <li><Link to="/Home">Home</Link></li>
+                  <li><Link to="/CityWeather">Прогноз погоды в городе</Link></li>
+                  <li><Link to="/LatLonWeather">Прогноз по координатам</Link></li>
+                  <li><Link to="#">About</Link></li>
+                  <li><Link to="#">Connect</Link></li>
+                </ul>
+              </div>
+            )}
           </nav>
         </div>
-           )}
+        
         <Routes>
           <Route path="/Home" element={<Home />} />
           <Route path="/CityWeather" element={<WeatherCity />} />
-          <Route path="/LatLonWeather" element={<WeatherLatLon/>}/>
+          <Route path="/LatLonWeather" element={<WeatherLatLon />} />
         </Routes>
-     
       </div>
-      
     </Router>
   );
 }
