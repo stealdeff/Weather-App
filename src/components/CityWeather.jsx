b2cf6adf8b39d25f12bc5d989c6c3064bc1b5490
Arina <Ukam.naletko@mail.ru>
2024-11-09 14:56:29 +0300

    IP-address added

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const WeatherCity = () => {
-  const [city, setCity] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
 
@@ -9,7 +9,7 @@ const WeatherCity = () => {
     e.preventDefault();
     try {
       const response = await fetch(
-        `http://api.weatherstack.com/current?access_key=99a1f5a417b2631da7765b170ca013f5&query=${encodeURIComponent(city)}`
+        `http://api.weatherstack.com/current?access_key=99a1f5a417b2631da7765b170ca013f5&query=${encodeURIComponent(searchTerm)}`
       );
       const data = await response.json();
       if (data.success === false) {
@@ -17,7 +17,7 @@ const WeatherCity = () => {
       }
 
       setWeatherData(data);
-      setError(null); 
+      setError(null);
     } catch (error) {
       console.error('Error when receiving weather data:', error);
       setError('Failed to download weather data');
@@ -31,8 +31,8 @@ const WeatherCity = () => {
     }
   };
 
-  const handleCityChange = (e) => {
-    setCity(e.target.value);
+  const handleSearchTermChange = (e) => {
+    setSearchTerm(e.target.value);
   };
 
   return (
@@ -40,39 +40,40 @@ const WeatherCity = () => {
       <form onSubmit={handleSubmit} onKeyDown={handleKeyDown}>
         <input
           type="text"
-          placeholder="Enter the name of the city"
-          value={city}
-          onChange={handleCityChange}
-          onKeyDown={handleKeyDown} 
+          placeholder="Enter the name of the city or IP address"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          onKeyDown={handleKeyDown}
         />
         <button type="submit">Show the weather</button>
       </form>
 
-      {error && <p style={{color: 'red'}}>{error}</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      {weatherData ? (
-        <div className='WeatherData'>
-         <h2>{weatherData.location.name}</h2>
-<ul className='weather_city'>
-  <li>Country: {weatherData.location.country}</li>
-  <li>Region: {weatherData.location.region}</li>
-  <li>Local time: {weatherData.location.localtime}</li>
-  <li>Temperature: {weatherData.current.temperature}°C</li>
-  <li>Weather description: {weatherData.current.weather_descriptions[0]}</li>
-  <li>Wind speed: {(weatherData.current.wind_speed * 2.237).toFixed(1)} mph</li>
-  <li>Wind direction: {weatherData.current.wind_dir}</li>
-  <li>Pressure: {(weatherData.current.pressure * 0.750062).toFixed(1)} mmHg</li>
-  <li>Humidity: {weatherData.current.humidity}%</li>
-  <li>Cloud cover: {weatherData.current.cloudcover}%</li>
-  <li>Feels like: {weatherData.current.feelslike}°C</li>
-  <li>UV Index: {weatherData.current.uv_index}</li>
-  <li>Visibility: {(weatherData.current.visibility * 0.621371).toFixed(1)} miles</li>
-</ul>
-        
+      {weatherData && (
+        <div className="WeatherData">
+          <h2>{weatherData.location.name || `IP address ${searchTerm}`}</h2>
+          <ul className="weather_city">
+            <li>Country: {weatherData.location.country}</li>
+            <li>Region: {weatherData.location.region}</li>
+            <li>Local time: {weatherData.location.localtime}</li>
+            <li>Temperature: {weatherData.current.temperature}°C</li>
+            <li>Weather description: {weatherData.current.weather_descriptions[0]}</li>
+            <li>Wind speed: {(weatherData.current.wind_speed * 2.237).toFixed(1)} mph</li>
+            <li>Wind direction: {weatherData.current.wind_dir}</li>
+            <li>Pressure: {(weatherData.current.pressure * 0.750062).toFixed(1)} mmHg</li>
+            <li>Humidity: {weatherData.current.humidity}%</li>
+            <li>Cloud cover: {weatherData.current.cloudcover}%</li>
+            <li>Feels like: {weatherData.current.feelslike}°C</li>
+            <li>UV Index: {weatherData.current.uv_index}</li>
+            <li>Visibility: {(weatherData.current.visibility * 0.621371).toFixed(1)} miles</li>
+          </ul>
+
+          <cite>Search by city name and IP-address</cite>
         </div>
-      ) : (
-        <p className='load'>Loading...</p>
       )}
+
+      {!weatherData && <p className="load">Loading...</p>}
     </div>
   );
 };
