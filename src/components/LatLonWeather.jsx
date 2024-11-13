9c3ac9d747dc5d8d717827dfd329da68de576d0a
Arina <Ukam.naletko@mail.ru>
2024-11-13 14:18:20 +0300

    i

--- src/components/LatLonWeather.jsx
+++ src/components/LatLonWeather.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // import  './Weather.css'
 const WeatherLatLon = () => {
     const [lat, setLat] = useState('');
